3ec341d32b65b1b530e4ee6c2087ce6e6e2ac5bc
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-04-24 15:27:20 +0300

    Add files via upload

---
+++ Programming-Basics-Exams/Programming-Basics-Online-Exam-20and21.April.2019/01. Easter Lunch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kozunakPrice = 3.20;
+            double eggPrice = 4.35;
+            double kyrabiqPrice = 5.40;
+            double eggPaintPrice = 0.15;
+
+            int kozunaciCount = int.Parse(Console.ReadLine());
+            int eggsCount = int.Parse(Console.ReadLine());
+            int kyrabiiKg = int.Parse(Console.ReadLine());
+
+            double totalKozynaci = kozunaciCount * kozunakPrice;
+            double totalEggs = eggsCount * eggPrice;
+            double totalKyrabii = kyrabiiKg * kyrabiqPrice;
+            double totalPaint = (eggsCount * 12) * eggPaintPrice;
+
+            double totalSum = totalKozynaci + totalEggs + totalKyrabii + totalPaint;
+            Console.WriteLine($"{totalSum:f2}");
+
+         
+
+        }
+    }
+}
