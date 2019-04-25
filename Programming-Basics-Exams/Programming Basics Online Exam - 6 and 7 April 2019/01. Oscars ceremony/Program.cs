543a16e1f5f4b870545a53ce6dc645e81feb4c08
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-04-25 11:41:05 +0300

    Add files via upload

---
+++ Programming-Basics-Exams/Programming Basics Online Exam - 6 and 7 April 2019/01. Oscars ceremony/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Oscars_ceremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+            double statuette = rent - (rent * 0.30);
+            double catering = statuette - (statuette * 0.15);
+            double sound = catering / 2;
+
+            double totalSum = rent + statuette + catering + sound;
+            Console.WriteLine($"{totalSum:f2}");
+
+        }
+    }
+}
