a182638c311cee95b77e35c8467ec644b9a23bb5
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-04-26 13:41:06 +0300

    Add files via upload

---
+++ Programming-Basics-Exams/Programming Basics Online Exam - 9 and 10 March 2019/06. High Jump/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06._High_Jump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = int.Parse(Console.ReadLine());
+
+            int intial = target - 30;
+            int jumpCounter = 0;
+            int currentTry = 0;
+            int currentJump = 0;
+
+            while (true)
+            {
+                for (currentTry = 1; currentTry <= 3; currentTry++)
+                {
+                    currentJump = int.Parse(Console.ReadLine());
+                    jumpCounter++;
+
+                    if (currentJump > intial)
+                    {
+                        if (intial >= target)
+                        {
+                            Console.WriteLine($"Tihomir succeeded, he jumped over {target}cm after {jumpCounter} jumps.");
+                            return;
+                        }
+
+                        intial += 5;
+                        break;
+                    }
+                }
+                if (currentTry == 4)
+                {
+                    Console.WriteLine($"Tihomir failed at {intial}cm after {jumpCounter} jumps.");
+                    return;
+                }
+            }
+        }
+    }
+}
