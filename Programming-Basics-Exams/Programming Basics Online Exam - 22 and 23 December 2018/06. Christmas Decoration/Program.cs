aa7a59088613d5f150a4511486d3c8b5093cdc2d
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-04-28 12:11:09 +0300

    Add files via upload

---
+++ Programming-Basics-Exams/Programming Basics Online Exam - 22 and 23 December 2018/06. Christmas Decoration/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06._Christmas_Decoration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var budget = double.Parse(Console.ReadLine());
+            bool left = true;
+            while (true)
+            {
+                string product = Console.ReadLine();
+                if (product == "Stop")
+                {
+                    break;
+                }
+                double price = 0;
+                for (int i = 0; i < product.Length; i++)
+                {
+                    price += product[i];
+                }
+                if (budget - price >= 0)
+                {
+                    budget -= price;
+                    Console.WriteLine("Item successfully purchased!");
+                }
+                else
+                {
+                    Console.WriteLine("Not enough money!");
+                    left = false;
+                    break;
+                }
+            }
+            if (left)
+            {
+                Console.WriteLine($"Money left: {budget}");
+            }
+        }
+    }
+}
