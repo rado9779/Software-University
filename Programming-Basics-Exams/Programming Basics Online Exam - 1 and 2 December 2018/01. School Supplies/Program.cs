abc3bcc1a94ad66e60fa096ccc83575bbed4f0c2
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-04-29 10:35:24 +0300

    Add files via upload

---
+++ Programming-Basics-Exams/Programming Basics Online Exam - 1 and 2 December 2018/01. School Supplies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._School_Supplies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pensCount = double.Parse(Console.ReadLine());
+            var markersCount = double.Parse(Console.ReadLine());
+            var deskCleanup = double.Parse(Console.ReadLine());
+            var discount = double.Parse(Console.ReadLine());
+
+            double penPrice = 5.80;
+            double markerPrice = 7.20;
+            double deskCleanupPrice = 1.20;
+
+            double totalSum = (pensCount * penPrice) + (markersCount * markerPrice) + (deskCleanup * deskCleanupPrice);
+            double sumWithDiscount = totalSum - (totalSum * (discount / 100));
+            Console.WriteLine($"{sumWithDiscount:f3}");
+        }
+    }
+}
