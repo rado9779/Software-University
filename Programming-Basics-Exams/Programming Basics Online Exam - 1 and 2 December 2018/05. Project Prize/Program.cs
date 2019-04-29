6ee40a40c7720f7949322ac0e62d84df9bf219af
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-04-29 12:03:43 +0300

    Add files via upload

---
+++ Programming-Basics-Exams/Programming Basics Online Exam - 1 and 2 December 2018/05. Project Prize/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Project_Prize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parts = int.Parse(Console.ReadLine());
+            double prize = double.Parse(Console.ReadLine());
+            int totalPoints = 0;
+            
+            for (int i = 1; i <= parts; i++)
+            {
+                int currentPoints = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    currentPoints *= 2;
+                }
+                totalPoints += currentPoints;
+            }
+
+            double totalSum = prize * totalPoints;
+            Console.WriteLine($"The project prize was {totalSum:f2} lv.");
+        }
+    }
+}
