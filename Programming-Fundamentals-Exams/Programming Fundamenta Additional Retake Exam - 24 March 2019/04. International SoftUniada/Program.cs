5853fc306da2484707aea3201253d2b3671ac518
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-04 14:03:37 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Exams/Programming Fundamenta Additional Retake Exam - 24 March 2019/04. International SoftUniada/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._SoftUniada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Dictionary<string, Dictionary<string, double>>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "END")
+                {
+                    break;
+                }
+                string[] splitted = line.Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+                string country = splitted[0];
+                string competitor = splitted[1];
+                double points = double.Parse(splitted[2]);
+                if (!result.ContainsKey(country))
+                {
+                    result.Add(country, new Dictionary<string, double>());
+                    result[country].Add(competitor, points);
+                }
+                else if (result.ContainsKey(country))
+                {
+                    if (result[country].ContainsKey(competitor))
+                    {
+                        result[country][competitor] += points;
+                    }
+                    else 
+                    {
+                        result[country].Add(competitor, points);
+                    }
+                }
+            }
+            foreach (var country in result.OrderByDescending(x => x.Value.Values.Sum()))
+            {
+                Console.WriteLine($"{country.Key}: {country.Value.Values.Sum()}");
+                foreach (var contestants in country.Value)
+                {
+                    Console.WriteLine($" -- {contestants.Key} -> {contestants.Value}");
+                }
+            }
+        }
+    }
+}
