0795e016b52cd3475aeb494bea7d2886ba8df88c
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-06 11:19:34 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Exams/Programming Fundamentals  Retake Exam - 28 October 2018/01. Google Searches/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Google_Searches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int users = int.Parse(Console.ReadLine());
+            double moneyPerSearch = double.Parse(Console.ReadLine());
+
+            double totalMoney = 0;
+
+            for (int i = 1; i <= users; i++)
+            {
+                int word = int.Parse(Console.ReadLine());
+                double money = 0;
+                if (word <= 5 && word > 0)
+                {
+                    money = moneyPerSearch;
+                }
+                if (word == 1)
+                {
+                    money = moneyPerSearch * 2;
+                }
+                if (i % 3 == 0)
+                {
+                    money *= 3;
+                }
+                totalMoney += money;
+            }
+            totalMoney *= days;
+            Console.WriteLine($"Total money earned for {days} days: {totalMoney:f2}");
+        }
+    }
+}
