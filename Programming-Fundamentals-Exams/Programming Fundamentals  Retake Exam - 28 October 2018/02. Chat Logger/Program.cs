8e2668f8bfeb6e25a8395f1f5d3a405b9d6301d9
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-06 11:50:44 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Exams/Programming Fundamentals  Retake Exam - 28 October 2018/02. Chat Logger/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Chat_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chat = new List<string>();
+
+            while (true)
+            {
+                string[] line = Console.ReadLine().Split();
+                string command = line[0];
+                if (command == "end")
+                {
+                    break;
+                }
+                else if (command == "Chat")
+                {
+                    string message = line[1];
+                    chat.Add(message);
+                }
+                else if (command == "Delete")
+                {
+                    string messageToDelete = line[1];
+                    if (chat.Contains(messageToDelete))
+                    {
+                        chat.Remove(messageToDelete);
+                    }
+                }
+                else if (command == "Edit")
+                {
+                    string messageToEdit = line[1];
+                    int index = chat.IndexOf(messageToEdit);
+                    string editedVersion = line[2];
+                    if (chat.Contains(messageToEdit))
+                    {
+                        chat[index] = editedVersion;
+                    }
+                }
+                else if (command == "Pin")
+                {
+                    string message = line[1];
+                    if (chat.Contains(message))
+                    {
+                        chat.Remove(message);
+                        chat.Add(message);
+                    }
+                }
+                else if (command == "Spam")
+                {
+                    for (int i = 1; i < line.Length; i++)
+                    {
+                        chat.Add(line[i]);
+                    }
+                }
+            }
+            foreach (var chats in chat)
+            {
+                Console.WriteLine(chats);
+            }
+        }
+    }
+}
