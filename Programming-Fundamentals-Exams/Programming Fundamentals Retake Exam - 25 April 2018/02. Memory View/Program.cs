f8bb74569daee403c095f10488bb055cc4c8aff5
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-14 11:45:59 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Exams/Programming Fundamentals Retake Exam - 25 April 2018/02. Memory View/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Memory_View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+            List<string> words = new List<string>();
+            
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "Visual Studio crash")
+                {
+                    break;
+                }
+                result += line + " ";
+            }
+            string[] splitted = result.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < splitted.Length - 5; i++)
+            {
+                if (splitted[i] == "32656"
+                    && splitted[i + 1] == "19759"
+                    && splitted[i + 2] == "32763"
+                    && splitted[i + 3] == "0"
+                    && splitted[i + 5] == "0")
+                {
+                    string word = "";
+                    int wordLenght = int.Parse(splitted[i + 4]);
+                    for (int j = i + 6; j < i + 6 + wordLenght; j++)
+                    {
+                        word += (char)int.Parse(splitted[j]);
+                    }
+                    words.Add(word);
+                }
+            }
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
