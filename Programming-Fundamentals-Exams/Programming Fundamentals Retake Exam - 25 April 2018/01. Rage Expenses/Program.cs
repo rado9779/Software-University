362195784f73eef638d2e075b8f434d30a3705a4
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-14 11:19:30 +0300

    Add files via upload

---
+++ Programming-Fundamentals-Exams/Programming Fundamentals Retake Exam - 25 April 2018/01. Rage Expenses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01._Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            double totalExpenses = 0;
+            int keyboardTrashes = 0;
+
+            for (int i = 1; i <= lostGames; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    totalExpenses += headsetPrice;
+                }
+                if (i % 3 == 0)
+                {
+                    totalExpenses += mousePrice;
+                }
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    totalExpenses += keyboardPrice;
+                    keyboardTrashes++;
+                    if (keyboardTrashes % 2 == 0)
+                    {
+                        totalExpenses += displayPrice;
+                    }
+                }
+            }
+            Console.WriteLine($"Rage expenses: {totalExpenses:f2} lv.");
+        }
+    }
+}
