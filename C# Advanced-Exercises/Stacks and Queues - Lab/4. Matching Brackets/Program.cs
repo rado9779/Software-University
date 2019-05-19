efd251b9216f9781b192686eb51966c6afc07c13
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-19 20:32:18 +0300

    Add files via upload

---
+++ C# Advanced-Exercises/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char ch = line[i];
+                if (ch == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (ch == ')')
+                {
+                    int startIndex = stack.Pop();
+                    string contents = line.Substring(startIndex, i - startIndex + 1);
+                    Console.WriteLine(contents);
+                }
+            }
+        }
+    }
+}
