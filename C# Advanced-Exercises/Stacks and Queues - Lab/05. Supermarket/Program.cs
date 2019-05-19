89ad0ba89dcc8b427f4c36f6661ee09a10974e08
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-19 20:34:13 +0300

    Add files via upload

---
+++ C# Advanced-Exercises/Stacks and Queues - Lab/05. Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5._Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<string>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line == "End")
+                {
+                    break;
+                }
+                if (line == "Paid")
+                {
+                    foreach (var customer in queue)
+                    {
+                        Console.WriteLine(customer);
+                    }
+                    queue.Clear();
+                }
+                else
+                {
+                    queue.Enqueue(line);
+                }
+            }
+            Console.WriteLine("{0} people remaining.", queue.Count);
+        }
+    }
+}
