874644b9955e2c248c6025a6d72d0e2ad8cd95d7
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-21 19:52:28 +0300

    Add files via upload

---
+++ C# Advanced-Exercises/Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<string>();
+            string text = string.Empty;
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string command = line[0];
+
+                if (command == "1")
+                {
+                    stack.Push(text);
+                    text += line[1];
+                }
+                else if (command == "2")
+                {
+                    stack.Push(text);
+                    int count = int.Parse(line[1]);
+                    text = text.Substring(0, text.Length - count);
+                }
+                else if (command == "3")
+                {
+                    int index = int.Parse(line[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (command == "4")
+                {
+                    text = stack.Pop();
+                }
+            }
+        }
+    }
+}
