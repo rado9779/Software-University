99b16889d0229c4db6d3936ff056415641cbecea
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-05-21 19:32:06 +0300

    Add files via upload

---
+++ C# Advanced-Exercises/Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] queue = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int command = queue[0];
+
+                if (command == 1)
+                {
+                    stack.Push(queue[1]);
+                }
+                if (command == 2)
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                if (command == 3)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                if (command == 4)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", stack));
+            }
+        }
+    }
+}
