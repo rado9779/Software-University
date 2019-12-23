6e036c9202b2a4d2b249ade5c6e976ba2b9b8f2b
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-12-23 15:33:26 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                int[] colELements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = colELements[col];
+                }
+            }
+
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
