e6ecece980b84776491828fb334f495ca2604998
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-12-25 11:13:08 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Multidimensional Arrays - Exercise/03. Maximal Sum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSizes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[,] matrix = new int[matrixSizes[0], matrixSizes[1]];
+
+            for (int row = 0; row < matrixSizes[0]; row++)
+            {
+                int[] colElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrixSizes[1]; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+
+            int maxSum = 0;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int row = 0; row < matrixSizes[0] - 2; row++)
+            {
+                for (int col = 0; col < matrixSizes[1] - 2; col++)
+                {
+                    int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] 
+                                   + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] 
+                                   + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        maxRow = row;
+                        maxCol = col;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int row = maxRow; row <= maxRow + 2; row++)
+            {
+                for (int col = maxCol; col <= maxCol + 2; col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
