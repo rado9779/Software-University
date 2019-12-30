51216f21d32c23d82c176605d58c75b8a83e3f27
rado9779 <49807042+rado9779@users.noreply.github.com>
2019-12-30 12:31:06 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Functional Programming - Exercise/01. Action Print/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> printNames = name =>
+              Console.WriteLine(string.Join(Environment.NewLine,name));
+
+            printNames(input);
+        }
+    }
+}
