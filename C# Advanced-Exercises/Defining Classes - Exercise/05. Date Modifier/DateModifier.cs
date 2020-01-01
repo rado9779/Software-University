81454fa9d396c9445cc2a0af9a143bce85821cbe
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-01 13:07:45 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime firstDate;
+        private DateTime secondDate;
+
+        public DateModifier()
+        {
+
+        }
+
+        public DateTime FirstDate { get; set; }
+
+        public DateTime SecondDate { get; set; }
+
+        public double DifferenceInDays(DateTime firstDate,DateTime secondDate)
+        {
+            var result = Math.Abs((firstDate - secondDate).TotalDays);
+            return result;
+        }
+    }
+}
