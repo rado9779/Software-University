21d81801bd8dc4702c2f1c74ca16c6ba872484da
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-01 15:13:17 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Defining Classes - Exercise/07. Raw Data/Cargo.cs
@@ -0,0 +1,18 @@
+﻿namespace RawData
+{
+    public class Cargo
+    {
+        private int cargoWeight;
+        private string cargoType;
+
+        public Cargo(int cargoWeight, string cargoType)
+        {
+            this.CargoWeight = cargoWeight;
+            this.CargoType = cargoType;
+        }
+
+        public int CargoWeight { get; set; }
+
+        public string CargoType { get; set; }
+    }
+}
