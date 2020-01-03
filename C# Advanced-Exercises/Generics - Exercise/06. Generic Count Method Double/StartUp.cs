b7868420c43ae71cfcf7809bae6152c9b3122799
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-03 12:04:47 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Generics - Exercise/06. Generic Count Method Double/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Box<double> box = new Box<double>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                double value = double.Parse(Console.ReadLine());
+                box.Add(value);
+            }
+
+            double element = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountOfGraterElements(box.Data, element));
+        }
+        public static int CountOfGraterElements<T>(List<T> box, double element)
+        {
+            int count = 0;
+
+            foreach (var boxElement in box)
+            {
+                if (element.CompareTo(boxElement) < 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
