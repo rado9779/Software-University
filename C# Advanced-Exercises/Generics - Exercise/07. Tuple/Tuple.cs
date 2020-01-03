06e1e20370397a2fa439acacdff2419f453b7e68
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-03 13:49:26 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Generics - Exercise/07. Tuple/Tuple.cs
@@ -0,0 +1,21 @@
+﻿namespace _07.Tuple
+{
+    public class Tuple<T1, T2>
+    {
+
+        private T1 tupleOne;
+        private T2 tupleTwo;
+
+        public Tuple(T1 tuple1, T2 tuple2)
+        {
+            this.tupleOne = tuple1;
+            this.tupleTwo = tuple2;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.tupleOne} -> {this.tupleTwo}";
+        }
+
+    }
+}
