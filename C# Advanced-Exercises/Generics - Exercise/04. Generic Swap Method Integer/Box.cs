b52726c56bd5deb5388a9caaf67b133231ec82af
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-03 11:23:53 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Generics - Exercise/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Values = new List<T>();
+        }
+
+        public List<T> Values { get; set; }
+
+        public int Count => this.Values.Count;
+
+        public void Add(T data)
+        {
+            this.Values.Add(data);
+        }
+    }
+}
