482d53eafa3bb8d8f019b5af8477b04408f667d3
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-03 11:57:26 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Generics - Exercise/05. Generic Count Method String/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.GenericCountMethodString
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+            this.Data = new List<T>();
+        }
+
+        public List<T> Data { get; set; }
+
+        public void Add(T value)
+        {
+            this.Data.Add(value);
+        }
+    }
+}
