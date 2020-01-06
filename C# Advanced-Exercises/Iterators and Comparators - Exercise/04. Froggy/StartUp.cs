cbc0bec7dc96eadbf80b9fb7191071318dfb9cfb
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-06 10:59:08 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Iterators and Comparators - Exercise/04. Froggy/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse));
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
