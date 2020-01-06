e1da6b98d60f47a064f446c42edd2a2eeac3049c
rado9779 <49807042+rado9779@users.noreply.github.com>
2020-01-06 10:42:10 +0200

    Add files via upload

---
+++ C# Advanced-Exercises/Iterators and Comparators - Exercise/03. Stack/Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private T[] array;
+        private int index;
+
+        public Stack(int initialCapacity = 4)
+        {
+            this.array = new T[initialCapacity];
+            this.index = 0;
+        }
+        public void Push(T element)
+        {
+            if (index == array.Length)
+            {
+                DoubleCapacity();
+            }
+            array[index++] = element;
+        }
+        public T Pop()
+        {
+            if (index == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            return array[--index];
+        }
+        private void DoubleCapacity()
+        {
+            T[] newArr = new T[array.Length * 2];
+            Array.Copy(array, newArr, array.Length);
+            array = newArr;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = index - 1; i >= 0; i--)
+            {
+                yield return array[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
