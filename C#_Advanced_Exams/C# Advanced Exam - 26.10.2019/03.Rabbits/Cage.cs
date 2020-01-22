3b7c96a237cbd1e340c899fe826f0841b2b158b7
rado9779 <radostin9779@gmail.com>
2020-01-22 12:02:37 +0200

    Exam folders are renamed

---
+++ C#_Advanced_Exams/C# Advanced Exam - 26.10.2019/03.Rabbits/Cage.cs
@@ -0,0 +1,71 @@
+﻿using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Rabbits
+{
+    public class Cage
+    {
+        private List<Rabbit> data;
+
+        public Cage(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.data = new List<Rabbit>();
+        }
+
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count();
+
+        public void Add(Rabbit rabbit)
+        {
+            if (this.data.Count < this.Capacity)
+            {
+                this.data.Add(rabbit);
+            }
+        }
+        public bool RemoveRabbit(string name)
+        {
+            Rabbit rabbit = this.data.FirstOrDefault(x => x.Name == name);
+
+            if (rabbit != null)
+            {
+                this.data.Remove(rabbit);
+                return true;
+            }
+            return false;
+        }
+        public void RemoveSpecies(string species)
+        {
+            this.data.RemoveAll(x => x.Species == species);
+        }
+        public Rabbit SellRabbit(string name)
+        {
+            Rabbit rabbit = this.data.FirstOrDefault(x => x.Name == name);
+            rabbit.Available = false;
+            return rabbit;
+        }
+        public Rabbit[] SellRabbitsBySpecies(string species)
+        {
+            List<Rabbit> rabbits = this.data.FindAll(x => x.Species == species);
+            rabbits.ForEach(x => x.Available = false);
+            return rabbits.ToArray();
+        }
+        public string Report()
+        {
+            List<Rabbit> rabbits = this.data.FindAll(x => x.Available == true);
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Rabbits available at {this.Name}:");
+            foreach (var rabbit in rabbits)
+            {
+                sb.AppendLine(rabbit.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#_Advanced_Exams/C# Advanced Retake Exam - 13.08.2019/01.SummerCocktails/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SummerCocktails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> cocktails = new Dictionary<string, int>()
+            {
+                {"Mimosa",150},
+                {"Daiquiri",250},
+                {"Sunshine",300},
+                {"Mojito",400}
+            };
+
+            Dictionary<string, int> readyCocktails = new Dictionary<string, int>();
+
+            Queue<int> ingredientValues = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Stack<int> freshnessLevels = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            while (ingredientValues.Count > 0 && freshnessLevels.Count > 0)
+            {
+                int ingredient = ingredientValues.Peek();
+
+                if (ingredient == 0)
+                {
+                    ingredientValues.Dequeue();
+                    continue;
+                }
+
+                int freshnessLevel = freshnessLevels.Peek();
+
+                int mixed = ingredient * freshnessLevel;
+
+                if (cocktails.ContainsValue(mixed))
+                {
+                    string cocktailName = cocktails.FirstOrDefault(x => x.Value == mixed).Key;
+
+                    if (readyCocktails.ContainsKey(cocktailName) == false)
+                    {
+                        readyCocktails.Add(cocktailName, 0);
+                    }
+                    readyCocktails[cocktailName]++;
+
+                    ingredientValues.Dequeue();
+                    freshnessLevels.Pop();
+                }
+                else
+                {
+                    freshnessLevels.Pop();
+                    int ingredientValue = ingredientValues.Dequeue();
+                    ingredientValues.Enqueue(ingredientValue + 5);
+                }
+            }
+            Console.WriteLine(HaveAllTypesCocktails(readyCocktails));
+            if (ingredientValues.Count > 0)
+            {
+                Console.WriteLine($"Ingredients left: {ingredientValues.Sum()}");
+            }
+            foreach (var cocktail in readyCocktails.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"# {cocktail.Key} --> {cocktail.Value}");
+            }
+        }
+        public static string HaveAllTypesCocktails(Dictionary<string, int> cocktails)
+        {
+            if (cocktails.ContainsKey("Mimosa") && cocktails.ContainsKey("Daiquiri") &&
+                cocktails.ContainsKey("Sunshine") && cocktails.ContainsKey("Mojito"))
+            {
+                return "It's party time! The cocktails are ready!";
+            }
+            return "What a pity! You didn't manage to prepare all cocktails.";
+        }
+    }
+}
