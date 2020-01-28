753a81b27edbc018dcdcf1b2daf1ea25a2ef72a0
rado9779 <radostin9779@gmail.com>
2020-01-28 12:39:12 +0200

    Add first and second task from 16.04.19 Exam

---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 16.04.2019/01.TrojanInvasion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.TrojanInvasion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waves = int.Parse(Console.ReadLine());
+
+            List<int> plates = new List<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Stack<int> warriors = new Stack<int>();
+
+            for (int wave = 1; wave <= waves; wave++)
+            {
+                warriors = new Stack<int>(Console.ReadLine()
+                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                   .Select(int.Parse));
+
+                if (wave % 3 == 0)
+                {
+                    int plate = int.Parse(Console.ReadLine());
+                    plates.Add(plate);
+                }
+
+                while (warriors.Count > 0 && plates.Count > 0)
+                {
+                    int warrior = warriors.Pop();
+                    int plate = plates[0];
+
+                    if (warrior > plate)
+                    {
+                        warriors.Push(warrior - plate);
+                        plates.RemoveAt(0);
+                    }
+                    else if (plate > warrior)
+                    {
+                        plate -= warrior;
+                        plates[0] = plate;
+                    }
+                    else
+                    {
+                        plates.RemoveAt(0);
+                    }
+                }
+                if (plates.Count == 0)
+                {
+                    break;
+                }
+            }
+
+            if (plates.Count > 0)
+            {
+                Console.WriteLine("The Spartans successfully repulsed the Trojan attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+            if (warriors.Count > 0)
+            {
+                Console.WriteLine("The Trojans successfully destroyed the Spartan defense.");
+                Console.WriteLine($"Warriors left: {string.Join(", ", warriors)}");
+            }
+        }
+    }
+}
---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 16.04.2019/02.Helen'sAbduction/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace _02.Helen_sAbduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parisEnergy = int.Parse(Console.ReadLine());
+            int fieldSize = int.Parse(Console.ReadLine());
+
+            char[][] field = new char[fieldSize][];
+            FillMatrix(field, fieldSize);
+
+            int[] parisPositions = FindParis(field, fieldSize);
+            int parisRow = parisPositions[0];
+            int parisCol = parisPositions[1];
+            bool isParisDead = false;
+
+            while (true)
+            {
+                int currentRow = parisRow;
+                int currentCol = parisCol;
+
+                string[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = line[0];
+                int spartanRow = int.Parse(line[1]);
+                int spartanCol = int.Parse(line[2]);
+
+                field[spartanRow][spartanCol] = 'S';
+                parisEnergy--;
+
+                field[parisRow][parisCol] = '-';
+
+                switch (direction)
+                {
+                    case "up":
+                        currentRow--;
+                        break;
+                    case "down":
+                        currentRow++;
+                        break;
+                    case "left":
+                        currentCol--;
+                        break;
+                    case "right":
+                        currentCol++;
+                        break;
+                }
+
+                if (ValidateIndeces(currentRow, currentCol, field, fieldSize) == true)
+                {
+                    parisRow = currentRow;
+                    parisCol = currentCol;
+                }
+
+                if (field[parisRow][parisCol] == 'S')
+                {
+                    parisEnergy -= 2;
+                    if (parisEnergy <= 0)
+                    {
+                        field[parisRow][parisCol] = 'X';
+                        isParisDead = true;
+                        break;
+                    }
+                    field[parisRow][parisCol] = 'P';
+                }
+                else if (field[parisRow][parisCol] == 'H')
+                {
+                    field[parisRow][parisCol] = '-';
+                    break;
+                }
+
+                if (parisEnergy <= 0)
+                {
+                    field[parisRow][parisCol] = 'X';
+                    isParisDead = true;
+                    break;
+                }
+            }
+
+            if (isParisDead == false)
+            {
+                Console.WriteLine($"Paris has successfully abducted Helen! Energy left: {parisEnergy}");
+            }
+            else
+            {
+                Console.WriteLine($"Paris died at {parisRow};{parisCol}.");
+            }
+            PrintMatrix(field, fieldSize);
+        }
+        public static void FillMatrix(char[][] matrix, int matrixSize)
+        {
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string colElements = Console.ReadLine();
+                matrix[row] = colElements.ToCharArray();
+            }
+        }
+        public static void PrintMatrix(char[][] matrix, int matrixSize)
+        {
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int[] FindParis(char[][] matrix, int matrixSize)
+        {
+            int[] result = new int[2];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] == 'P')
+                    {
+                        result[0] = row;
+                        result[1] = col;
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+        public static bool ValidateIndeces(int row, int col, char[][] matrix, int matrixSize)
+        {
+            if (row >= 0 && row < matrixSize && col >= 0 && col < matrix[row].Length)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
