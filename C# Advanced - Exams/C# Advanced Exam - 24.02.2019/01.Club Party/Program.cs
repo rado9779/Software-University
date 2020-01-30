0bb7ad7081940cde14ce202c0c673a1b6df3cba2
rado9779 <radostin9779@gmail.com>
2020-01-30 18:32:46 +0200

    Add C# advanced exam - 24.02.19

---
+++ C# Advanced - Exams/C# Advanced Exam - 24.02.2019/01.Club Party/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hallsCapacity = int.Parse(Console.ReadLine());
+
+            Stack<string> line = new Stack<string>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            List<string> halls = new List<string>();
+            List<int> reservations = new List<int>();
+
+            int currentPeople = 0;
+
+            while (line.Count > 0)
+            {
+                string element = line.Pop();
+
+                if (char.IsLetter(element[0]))
+                {
+                    halls.Add(element);
+                }
+                else
+                {
+                    int guests = int.Parse(element);
+
+                    if (halls.Count == 0)
+                    {
+                        continue;
+                    }
+                    if (currentPeople + guests > hallsCapacity)
+                    {
+                        Console.WriteLine($"{halls[0]} -> {string.Join(", ", reservations)}");
+                        halls.RemoveAt(0);
+                        reservations.Clear();
+                        currentPeople = 0;
+                    }
+                    if (halls.Count > 0)
+                    {
+                        reservations.Add(guests);
+                        currentPeople += guests;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced - Exams/C# Advanced Exam - 24.02.2019/02.TronRacers/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+
+namespace _02.TronRacers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+            FillMatrix(matrix, matrixSize);
+
+            int[] firstPlayerPosition = FindPlayer(matrix, matrixSize, 'f');
+            int firstPlayerRow = firstPlayerPosition[0];
+            int firstPlayerCol = firstPlayerPosition[1];
+
+            int[] secondPlayerPosition = FindPlayer(matrix, matrixSize, 's');
+            int secondPlayerRow = secondPlayerPosition[0];
+            int secondPlayerCol = secondPlayerPosition[1];
+
+            while (true)
+            {
+                string[] commands = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string firstCommand = commands[0];
+                string secondCommand = commands[1];
+
+                matrix[firstPlayerRow, firstPlayerCol] = 'f';
+                matrix[secondPlayerRow, secondPlayerCol] = 's';
+
+                if (firstCommand == "up")
+                {
+                    firstPlayerRow--;
+                    if (firstPlayerRow < 0)
+                    {
+                        firstPlayerRow = matrixSize - 1;
+                    }
+                    if (FirstPlayerGo(matrix, firstPlayerRow, firstPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                else if (firstCommand == "down")
+                {
+                    firstPlayerRow++;
+                    if (firstPlayerRow >= matrixSize)
+                    {
+                        firstPlayerRow = 0;
+                    }
+                    if (FirstPlayerGo(matrix, firstPlayerRow, firstPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                else if (firstCommand == "left")
+                {
+                    firstPlayerCol--;
+                    if (firstPlayerCol < 0)
+                    {
+                        firstPlayerCol = matrixSize - 1;
+                    }
+                    if (FirstPlayerGo(matrix, firstPlayerRow, firstPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                else if (firstCommand == "right")
+                {
+                    firstPlayerCol++;
+                    if (firstPlayerCol >= matrixSize)
+                    {
+                        firstPlayerCol = 0;
+                    }
+                    if (FirstPlayerGo(matrix, firstPlayerRow, firstPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                //second
+                if (secondCommand == "up")
+                {
+                    secondPlayerRow--;
+                    if (secondPlayerRow < 0)
+                    {
+                        secondPlayerRow = matrixSize - 1;
+                    }
+                    if (SecondPlayerGo(matrix, secondPlayerRow, secondPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                else if (secondCommand == "down")
+                {
+                    secondPlayerRow++;
+                    if (secondPlayerRow >= matrixSize)
+                    {
+                        secondPlayerRow = 0;
+                    }
+                    if (SecondPlayerGo(matrix, secondPlayerRow, secondPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                else if (secondCommand == "left")
+                {
+                    secondPlayerCol--;
+                    if (secondPlayerCol < 0)
+                    {
+                        secondPlayerCol = matrixSize - 1;
+                    }
+                    if (SecondPlayerGo(matrix, secondPlayerRow, secondPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+                else if (secondCommand == "right")
+                {
+                    secondPlayerCol++;
+                    if (secondPlayerCol >= matrixSize)
+                    {
+                        secondPlayerCol = 0;
+                    }
+                    if (SecondPlayerGo(matrix, secondPlayerRow, secondPlayerCol) == true)
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            PrintMatrix(matrix, matrixSize);
+
+        }
+        public static void FillMatrix(char[,] matrix, int matrixSize)
+        {
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string colElements = Console.ReadLine();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+        }
+        public static void PrintMatrix(char[,] matrix, int matrixSize)
+        {
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int[] FindPlayer(char[,] matrix, int matrixSize, char playerSymbol)
+        {
+            int[] playerCoordinates = new int[2];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    if (matrix[row, col] == playerSymbol)
+                    {
+                        playerCoordinates[0] = row;
+                        playerCoordinates[1] = col;
+                    }
+                }
+            }
+            return playerCoordinates;
+        }
+        public static bool FirstPlayerGo(char[,] matrix,int row, int col)
+        {
+            if (matrix[row,col] == 's')
+            {
+                matrix[row, col] = 'x';
+            }
+            return true;
+        }
+        public static bool SecondPlayerGo(char[,] matrix, int row, int col)
+        {
+            if (matrix[row, col] == 'f')
+            {
+                matrix[row, col] = 'x';
+            }
+            return true;
+        }
+    }
+}
