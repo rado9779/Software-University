dbc5da9a0213f46b3a723ad283b41247f2f2bf9e
rado9779 <radostin9779@gmail.com>
2020-02-01 18:57:05 +0200

    Add 17.12.19 Exam

---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 17.12.2019/01.Santa'sPresentFactory/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Santa_sPresentFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> table = new Dictionary<int, string>()
+            {
+                {150,"Doll"},
+                {250,"Wooden train"},
+                {300,"Teddy bear"},
+                {400,"Bicycle"}
+            };
+
+            SortedDictionary<string, int> presents = new SortedDictionary<string, int>();
+
+            Stack<int> materials = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Queue<int> magicLevels = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            while (materials.Count > 0 && magicLevels.Count > 0)
+            {
+                int material = materials.Peek();
+                int magicLevel = magicLevels.Peek();
+
+                if (material == 0)
+                {
+                    materials.Pop();
+                }
+                if (magicLevel == 0)
+                {
+                    magicLevels.Dequeue();
+                }
+
+                int totalMagicLevel = material * magicLevel;
+
+                if (table.ContainsKey(totalMagicLevel))
+                {
+                    if (presents.ContainsKey(table[totalMagicLevel]) == false)
+                    {
+                        presents.Add(table[totalMagicLevel], 0);
+                    }
+                    presents[table[totalMagicLevel]]++;
+                    materials.Pop();
+                    magicLevels.Dequeue();
+                }
+                if (totalMagicLevel < 0)
+                {
+                    int sum = material + magicLevel;
+                    materials.Pop();
+                    magicLevels.Dequeue();
+                    materials.Push(sum);
+                }
+                if (table.ContainsKey(totalMagicLevel) == false && totalMagicLevel > 0)
+                {
+                    magicLevels.Dequeue();
+                    materials.Push(materials.Pop() + 15);
+                }
+            }
+
+            if (presents.ContainsKey("Doll") && presents.ContainsKey("Wooden train") ||
+                presents.ContainsKey("Teddy bear") && presents.ContainsKey("Bicycle"))
+            {
+                Console.WriteLine("The presents are crafted! Merry Christmas!");
+            }
+            else
+            {
+                Console.WriteLine("No presents this Christmas!");
+            }
+
+            if (materials.Count > 0)
+            {
+                Console.WriteLine($"Materials left: {string.Join(", ", materials)}");
+            }
+            if (magicLevels.Count > 0)
+            {
+                Console.WriteLine($"Magic left: {string.Join(", ", magicLevels)}");
+            }
+
+
+            foreach (var (toy, amount) in presents.Where(x => x.Value > 0))
+            {
+                Console.WriteLine($"{toy}: {amount}");
+            }
+        }
+    }
+}
---
+++ C# Advanced - Exams/C# Advanced Retake Exam - 17.12.2019/02.PresentDelivery/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.PresentDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int presents = int.Parse(Console.ReadLine());
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+            FillMatrix(matrix, matrixSize);
+
+            int[] santaPosition = FindSanta(matrix, matrixSize);
+            int santaRow = santaPosition[0];
+            int santaCol = santaPosition[1];
+
+            int happyKids = 0;
+
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "Christmas morning")
+                {
+                    break;
+                }
+                matrix[santaRow, santaCol] = '-';
+
+                switch (command)
+                {
+                    case "up":
+                        santaRow--;
+                        break;
+                    case "down":
+                        santaRow++;
+                        break;
+                    case "left":
+                        santaCol--;
+                        break;
+                    case "right":
+                        santaCol++;
+                        break;
+                }
+
+                if (matrix[santaRow, santaCol] == 'V')
+                {
+                    presents--;
+                    matrix[santaRow, santaCol] = '-';
+                    happyKids++;
+                }
+                else if (matrix[santaRow, santaCol] == 'X')
+                {
+                    matrix[santaRow, santaCol] = '-';
+                }
+                else if (matrix[santaRow, santaCol] == 'C')
+                {
+                    matrix[santaRow, santaCol] = '-';
+                    if (matrix[santaRow - 1,santaCol] == 'V' || )
+                    {
+
+                    }
+
+                }
+                matrix[santaRow, santaCol] = 'S';
+                if (presents == 0)
+                {
+                    Console.WriteLine("Santa ran out of presents!");
+                    break;
+                }
+                if (KidsWithoutPresent(matrix, matrixSize) == 0)
+                {
+                    break;
+                }
+            }
+
+            PrintMatrix(matrix, matrixSize);
+            if (KidsWithoutPresent(matrix, matrixSize) == 0)
+            {
+                Console.WriteLine($"Good job, Santa! {happyKids} happy nice kid/s.");
+            }
+            else
+            {
+                int kids = KidsWithoutPresent(matrix, matrixSize);
+                Console.WriteLine($"No presents for {kids} nice kid/s.");
+            }
+
+        }
+        public static void FillMatrix(char[,] matrix, int matrixSize)
+        {
+            for (int row = 0; row < matrixSize; row++)
+            {
+                char[] colElements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+        }
+        public static void PrintMatrix(char[,] matrix, int matrixSize)
+        {
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int[] FindSanta(char[,] matrix, int matrixSize)
+        {
+            int[] coordinates = new int[2];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    if (matrix[row, col] == 'S')
+                    {
+                        coordinates[0] = row;
+                        coordinates[1] = col;
+                    }
+                }
+            }
+            return coordinates;
+        }
+        public static int KidsWithoutPresent(char[,] matrix, int matrixSize)
+        {
+            int kids = 0;
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    if (matrix[row, col] == 'V')
+                    {
+                        kids++;
+                    }
+                }
+            }
+            return kids;
+        }
+        public static bool GivePresent(int row, int col, char[,] matrix)
+        {
+            if (matrix[row, col] == 'C' || matrix[row, col] == 'X')
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
