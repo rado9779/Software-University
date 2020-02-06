485ce05e73a9914c747acc528f393dbe9dccd3b3
rado9779 <radostin9779@gmail.com>
2020-02-06 10:57:15 +0200

    rename folder

---
+++ C# OOP - Exercises/WorkingWithAbstraction - Exercise/P04_Hospital/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04_Hospital
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, List<string>> doctors = new Dictionary<string, List<string>>();
+            Dictionary<string, List<List<string>>> departments = new Dictionary<string, List<List<string>>>();
+
+
+            string command = Console.ReadLine();
+            while (command != "Output")
+            {
+                string[] elements = command.Split();
+                var departament = elements[0];
+                var firstName = elements[1];
+                var lastName = elements[2];
+                var patient = elements[3];
+                var fullName = firstName + lastName;
+
+                if (!doctors.ContainsKey(fullName))
+                {
+                    doctors[fullName] = new List<string>();
+                }
+                if (!departments.ContainsKey(departament))
+                {
+                    departments[departament] = new List<List<string>>();
+                    for (int rooms = 0; rooms < 20; rooms++)
+                    {
+                        departments[departament].Add(new List<string>());
+                    }
+                }
+
+                bool havePlace = departments[departament]
+                    .SelectMany(x => x)
+                    .Count() < 60;
+
+                if (havePlace)
+                {
+                    int room = 0;
+                    doctors[fullName].Add(patient);
+
+                    for (int i = 0; i < departments[departament].Count; i++)
+                    {
+                        if (departments[departament][i].Count < 3)
+                        {
+                            room = i;
+                            break;
+                        }
+                    }
+                    departments[departament][room].Add(patient);
+                }
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] elements = command.Split();
+
+                if (elements.Length == 1)
+                {
+                    Console.WriteLine(string.Join("\n", departments[elements[0]].Where(x => x.Count > 0).SelectMany(x => x)));
+                }
+                else if (elements.Length == 2 && int.TryParse(elements[1], out int staq))
+                {
+                    Console.WriteLine(string.Join("\n", departments[elements[0]][staq - 1].OrderBy(x => x)));
+                }
+                else
+                {
+                    Console.WriteLine(string.Join("\n", doctors[elements[0] + elements[1]].OrderBy(x => x)));
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ C# OOP - Exercises/WorkingWithAbstraction - Exercise/P05_GreedyTimes/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_GreedyTimes
+{
+
+    public class Potato
+    {
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            string[] safe = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var bag = new Dictionary<string, Dictionary<string, long>>();
+            long gold = 0;
+            long gems = 0;
+            long money = 0;
+
+            for (int i = 0; i < safe.Length; i += 2)
+            {
+                string name = safe[i];
+                long count = long.Parse(safe[i + 1]);
+
+                string element = string.Empty;
+
+                if (name.Length == 3)
+                {
+                    element = "Cash";
+                }
+                else if (name.ToLower().EndsWith("gem"))
+                {
+                    element = "Gem";
+                }
+                else if (name.ToLower() == "gold")
+                {
+                    element = "Gold";
+                }
+
+                if (element == "")
+                {
+                    continue;
+                }
+                else if (input < bag.Values.Select(x => x.Values.Sum()).Sum() + count)
+                {
+                    continue;
+                }
+
+                switch (element)
+                {
+                    case "Gem":
+                        if (!bag.ContainsKey(element))
+                        {
+                            if (bag.ContainsKey("Gold"))
+                            {
+                                if (count > bag["Gold"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[element].Values.Sum() + count > bag["Gold"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                    case "Cash":
+                        if (!bag.ContainsKey(element))
+                        {
+                            if (bag.ContainsKey("Gem"))
+                            {
+                                if (count > bag["Gem"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[element].Values.Sum() + count > bag["Gem"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                if (!bag.ContainsKey(element))
+                {
+                    bag[element] = new Dictionary<string, long>();
+                }
+
+                if (!bag[element].ContainsKey(name))
+                {
+                    bag[element][name] = 0;
+                }
+
+                bag[element][name] += count;
+                if (element == "Gold")
+                {
+                    gold += count;
+                }
+                else if (element == "Gem")
+                {
+                    gems += count;
+                }
+                else if (element == "Cash")
+                {
+                    money += count;
+                }
+            }
+
+            foreach (var kvp in bag)
+            {
+                Console.WriteLine($"<{kvp.Key}> ${kvp.Value.Values.Sum()}");
+                foreach (var value in kvp.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+                {
+                    Console.WriteLine($"##{value.Key} - {value.Value}");
+                }
+            }
+        }
+    }
+}
