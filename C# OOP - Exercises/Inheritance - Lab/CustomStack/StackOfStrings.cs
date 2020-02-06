c430eb2f110b579b95343aa88a31775113289f2f
rado9779 <radostin9779@gmail.com>
2020-02-06 10:57:44 +0200

    Add Inheritance - Lab Folder

---
+++ C# OOP - Exercises/Inheritance - Lab/CustomStack/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+        public void AddRange(Stack<string> stack)
+        {
+            foreach (var element in stack)
+            {
+                this.Push(element);
+            }
+        }
+    }
+}
