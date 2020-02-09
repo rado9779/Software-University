e56331cebe8fad5bb59540b6bb2f6cabbea8137e
rado9779 <radostin9779@gmail.com>
2020-02-09 11:11:07 +0200

    Add Encapsulation - Lab folder

---
+++ C# OOP - Exercises/Encapsulation - Lab/PersonsInfo/StartUp .cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //First Problem
+
+            //var lines = int.Parse(Console.ReadLine());
+            //var persons = new List<Person>();
+            //for (int i = 0; i < lines; i++)
+            //{
+            //    var cmdArgs = Console.ReadLine().Split();
+            //    var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+            //    persons.Add(person);
+            //}
+
+            //persons.OrderBy(p => p.FirstName)
+            //       .ThenBy(p => p.Age)
+            //       .ToList()
+            //       .ForEach(p => Console.WriteLine(p.ToString()));
+
+
+            //Second Problem
+            //var lines = int.Parse(Console.ReadLine());
+            //var persons = new List<Person>();
+            //for (int i = 0; i < lines; i++)
+            //{
+            //    var cmdArgs = Console.ReadLine().Split();
+            //    var person = new Person(cmdArgs[0],
+            //                            cmdArgs[1],
+            //                            int.Parse(cmdArgs[2]),
+            //                            decimal.Parse(cmdArgs[3]));
+
+            //    persons.Add(person);
+            //}
+            //var parcentage = decimal.Parse(Console.ReadLine());
+            //persons.ForEach(p => p.IncreaseSalary(parcentage));
+            //persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            //Third Problem
+            //var lines = int.Parse(Console.ReadLine());
+            //var persons = new List<Person>();
+            //for (int i = 0; i < lines; i++)
+            //{
+            //    try
+            //    {
+            //        var cmdArgs = Console.ReadLine().Split();
+            //        var person = new Person(cmdArgs[0],
+            //                                cmdArgs[1],
+            //                                int.Parse(cmdArgs[2]),
+            //                                decimal.Parse(cmdArgs[3]));
+            //        persons.Add(person);
+            //    }
+            //    catch (ArgumentException ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+            //var parcentage = decimal.Parse(Console.ReadLine());
+            //persons.ForEach(p => p.IncreaseSalary(parcentage));
+            //persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+            //Fourth Problem
+            //Team team = new Team("SoftUni");
+
+            //foreach (Person person in persons)
+            //{
+            //    team.AddPlayer(person);
+            //}
+
+            //Console.WriteLine(team);
+        }
+    }
+}
---
+++ C# OOP - Exercises/Encapsulation - Lab/PersonsInfo/Team.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        public string Name { get; set; }
+        public  IReadOnlyCollection<Person> FirstTeam { get; private set; }
+        public IReadOnlyCollection<Person> ReserveTeam { get; private set; }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"First team has {firstTeam.Count} players.");
+            sb.AppendLine($"Reserve team has {reserveTeam.Count} players.");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
