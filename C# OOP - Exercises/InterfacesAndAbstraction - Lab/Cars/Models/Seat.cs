8e864fd34c4fa4748e510226a8e920948fd998d4
rado9779 <radostin9779@gmail.com>
2020-02-22 10:54:02 +0200

    Add InterfacesAndAbstraction - Lab Folder

---
+++ C# OOP - Exercises/InterfacesAndAbstraction - Lab/Cars/Models/Seat.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+using Cars.Interfaces;
+
+namespace Cars.Models
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return $"Breaaak!";
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} Seat {this.Model}");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
