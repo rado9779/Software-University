2f8fa5bef0b80123892ed9c166bc3be4c356f8b9
rado9779 <radostin9779@gmail.com>
2020-09-14 11:29:59 +0300

    Add Reflection and Attributes - Lab

---
+++ C# OOP - Exercises/Reflection and Attributes - Lab/Stealer/StartUp .cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+
+            //string result = spy.StealFieldInfo("Stealer.Hacker","username","password");
+            //string result = spy.AnalyzeAcessModifers("Stealer.Hacker");
+            //string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
